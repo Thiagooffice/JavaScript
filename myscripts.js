8430fe60f5a74687cac47687dbffac05d6f41e46
Thiago <thiagooffice2@gmail.com>
2021-08-19 15:34:30 -0300

    Aprendendo ES6

---
+++ myscripts.js
@@ -0,0 +1,71 @@
+//Aprendendo ES6
+
+/*function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar"
+}
+function redirecionar(){
+    window.open("https://www.google.com.br/")
+    //window.location.href =" https://www.google.com.br/"
+}
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse"
+    //alert("trocar texto")
+}
+function voltar(){
+    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui"}*/
+
+
+
+/*function soma(n1, n2){
+    return n1 + n2
+}
+alert(soma(10, 10))
+
+//var d = new Date()
+//alert(d.getMonth()+1)
+//alert(d.getDay())
+//alert(d.getHours())
+//alert(d.getMinutes())
+
+/*var count
+for(count = 0; count <= 10; count++){
+    console.log(count)
+}*/
+
+/*var count = 0
+while (count <= 20){
+    console.log(count)
+    count++
+}*/
+
+/*var idade = prompt("Qual a sua idade?")
+if(idade >= 18){
+    alert("Maior de idade")
+} else{
+    alert("Menor de idade")
+}
+
+/*var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas)
+alert(frutas[0].cor)*/
+
+/*var fruta = {nome:"maçã", cor:"vermelha"}
+console.log(fruta.nome)
+alert(fruta.cor)*/
+
+//var lista = ["maça", "pera", "laranja"]
+//lista.push("uva")
+//lista.pop()
+//console.log(lista)
+//console.log(lista.toString())
+//console.log(lista.join(" . "))
+
+/*var nome = "Thiago leal de souza"
+var idade = 29
+var idade2 = 10
+var frase = "Brasil é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos ")
+//alert(idade + idade2)
+console.log(nome)
+console.log(idade + idade2)
+console.log(frase.replace("Brasil", "Japão"))*/
